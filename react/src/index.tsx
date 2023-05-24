dfa31213ca1adc48a6526487dd8adedd150c90c2
Skeletonxf <skeletonxf@gmail.com>
2023-05-24 20:45:09 +0100

    Migrate from React 17 setup method to 18 version

--- react/src/index.tsx
+++ react/src/index.tsx
@@ -1,19 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { Provider } from "react-redux"
 
-import App from "./App";
-import { store } from "./store/store";
+import App from './App'
+import { store } from './store/store'
 
+const container = document.getElementById('root')
+const root = createRoot(container!)
 /**
  * Tells React what to render and where to render it.
  *
  * In our case, we're rending our root `App` component to the DOM element with
  * the id of `root` in the `public/index.html` file.
  */
-ReactDOM.render(
+root.render(
     <Provider store={store}>
         <App />
-    </Provider>,
-    document.getElementById("root"),
-);
+    </Provider>
+)
